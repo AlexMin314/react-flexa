e3254f6919895e0549d6a43981df000717688dc2
AlexMin314 <m98stone@gmail.com>
2018-09-18 15:32:05 +0800

    flex proptype changes

--- src/components/Col/Col.js
+++ src/components/Col/Col.js
@@ -1,73 +1,89 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
-import { themeProvider } from '../../theme';
-import { filterProps, mediaQuery, columnWidth, gutter, CSSProperty } from '../../helpers';
+import { themeProvider } from "../../theme";
+import {
+  filterProps,
+  mediaQuery,
+  columnWidth,
+  gutter,
+  CSSProperty
+} from "../../helpers";
 
 const Col = styled(props =>
-  React.createElement(props.elementType, filterProps(props, Col.propTypes)),
+  React.createElement(props.elementType, filterProps(props, Col.propTypes))
 )`
   // Initial component properties
   box-sizing: border-box;
 
-  ${props => themeProvider.breakpointsKeys(props).map(breakpoint => mediaQuery(props)[breakpoint]`
+  ${props =>
+    themeProvider.breakpointsKeys(props).map(
+      breakpoint => mediaQuery(props)[breakpoint]`
     // Generated Display
-    ${CSSProperty(props, breakpoint, 'display')}
+    ${CSSProperty(props, breakpoint, "display")}
 
     // Generate gutter
     ${gutter.col(props, breakpoint)}
 
     // Generate flex rule before width, this avoid override
-    ${CSSProperty(props, breakpoint, 'flex')}
+    ${CSSProperty(props, breakpoint, "flex")}
 
     // Generate column width
     ${columnWidth(props, breakpoint)}
 
     // Responsive Flexbox properties
-    ${CSSProperty(props, breakpoint, 'order')}
-    ${CSSProperty(props, breakpoint, 'align-self')}
-  `)}
+    ${CSSProperty(props, breakpoint, "order")}
+    ${CSSProperty(props, breakpoint, "align-self")}
+  `
+    )};
 `;
 
 Col.defaultProps = {
   order: 0,
-  alignSelf: 'auto',
-  elementType: 'div',
-  display: 'block',
-  flex: '0 0 auto',
+  alignSelf: "auto",
+  elementType: "div",
+  display: "block",
+  flex: "0 0 auto"
 };
 
-const displayOptions = ['flex', 'inline-flex', 'block', 'none', 'inline-block'];
-export const alignSelfOptions = ['auto', 'flex-start', 'flex-end', 'center', 'baseline', 'stretch'];
+const displayOptions = ["flex", "inline-flex", "block", "none", "inline-block"];
+export const alignSelfOptions = [
+  "auto",
+  "flex-start",
+  "flex-end",
+  "center",
+  "baseline",
+  "stretch"
+];
 
 Col.propTypes = {
-  xs: PropTypes.oneOfType([PropTypes.number, PropTypes.oneOf(['hidden', 'auto'])]),
-  sm: PropTypes.oneOfType([PropTypes.number, PropTypes.oneOf(['hidden', 'auto'])]),
-  md: PropTypes.oneOfType([PropTypes.number, PropTypes.oneOf(['hidden', 'auto'])]),
-  lg: PropTypes.oneOfType([PropTypes.number, PropTypes.oneOf(['hidden', 'auto'])]),
+  xs: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.oneOf(["hidden", "auto"])
+  ]),
+  sm: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.oneOf(["hidden", "auto"])
+  ]),
+  md: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.oneOf(["hidden", "auto"])
+  ]),
+  lg: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.oneOf(["hidden", "auto"])
+  ]),
 
   gutter: PropTypes.oneOfType([
     PropTypes.number,
     PropTypes.string,
     PropTypes.shape({
-      xs: PropTypes.oneOfType([
-        PropTypes.number,
-        PropTypes.string,
-      ]),
-      sm: PropTypes.oneOfType([
-        PropTypes.number,
-        PropTypes.string,
-      ]),
-      md: PropTypes.oneOfType([
-        PropTypes.number,
-        PropTypes.string,
-      ]),
-      lg: PropTypes.oneOfType([
-        PropTypes.number,
-        PropTypes.string,
-      ]),
-    }),
+      xs: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+      sm: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+      md: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+      lg: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+    })
   ]),
 
   order: PropTypes.oneOfType([
@@ -76,8 +92,8 @@ Col.propTypes = {
       xs: PropTypes.number,
       sm: PropTypes.number,
       md: PropTypes.number,
-      lg: PropTypes.number,
-    }),
+      lg: PropTypes.number
+    })
   ]),
 
   alignSelf: PropTypes.oneOfType([
@@ -86,8 +102,8 @@ Col.propTypes = {
       xs: PropTypes.oneOf(alignSelfOptions),
       sm: PropTypes.oneOf(alignSelfOptions),
       md: PropTypes.oneOf(alignSelfOptions),
-      lg: PropTypes.oneOf(alignSelfOptions),
-    }),
+      lg: PropTypes.oneOf(alignSelfOptions)
+    })
   ]),
 
   elementType: PropTypes.string,
@@ -95,6 +111,12 @@ Col.propTypes = {
   flex: PropTypes.oneOfType([
     PropTypes.number,
     PropTypes.string,
+    PropTypes.shape({
+      xs: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+      sm: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+      md: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+      lg: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+    })
   ]),
 
   display: PropTypes.oneOfType([
@@ -103,13 +125,13 @@ Col.propTypes = {
       xs: PropTypes.oneOf(displayOptions),
       sm: PropTypes.oneOf(displayOptions),
       md: PropTypes.oneOf(displayOptions),
-      lg: PropTypes.oneOf(displayOptions),
-    }),
+      lg: PropTypes.oneOf(displayOptions)
+    })
   ]),
 
-  children: PropTypes.node,
+  children: PropTypes.node
 };
 
-Col.displayName = 'Col';
+Col.displayName = "Col";
 
 export default Col;
